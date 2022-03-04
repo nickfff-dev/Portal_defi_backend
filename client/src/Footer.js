c1069b0a3be9e4b8114a40cd7d9aa48fb6c354f5
nickfff-dev <hunchographics@gmail.com>
2022-03-05 02:15:58 +0300

    as

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -15,6 +15,9 @@ class Footer extends Component {
                 <li><a href="https://t.me/getportal" ><i className="fa-brands fa-telegram"></i></a></li>
                 <li><a href="https://medium.com/@Portal_team" ><i className="fa-brands fa-medium"></i></a></li>
                 <li><a href="https://www.linkedin.com/company/18201464" ><i className="fa-brands fa-linkedin-in"></i></a></li>
+                <li><a href="https://PortalFinance/" ><i className="fa fa-user-astronaut"></i></a></li>
+                <li><a href="https://discord.com" ><i className="fa-brands fa-discord"></i></a></li>
+                <li><a href="https://www.reddit.com/r/PortalFinance/" ><i className="fa">&#xf4fb;</i></a></li>
               </ul>
               <div className="wrapper" style={{textAlign: "center", width:"100%", opacity: "0.6"}}>
                 <p className="small">&copy; 2022 Tides.Network Inc.  All rights reserved.</p>
